7ac7843a019996837ad56e781178612f1de706d3
edwin kim <emoney86@gmail.com>
2014-11-26 19:38:29 -0500

    Change function name from getElements to getTime.

--- pinpoint-items-from-dom-extractor.js
+++ pinpoint-items-from-dom-extractor.js
@@ -2,8 +2,11 @@ PinPoint.ItemsfromDomExtractor = function(targetName) {
   this.targetName = targetName;
 }
 
+// We'll need logic stating what className to use depending on sit:
+// YouTube: "ytp-time-current"
+// Vimeo: "box"
 PinPoint.ItemsfromDomExtractor.prototype = {
-  getElements : function(element) {
+  getTime : function(className) {
     var timeDivArray = document.getElementsbyClassName("ytp-time-current")
     var time = timeDivArray[0].innerHTML
 
