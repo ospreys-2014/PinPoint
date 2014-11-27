95aaeee89de8dd829149f032e71dfe30067c386b
edwin kim <emoney86@gmail.com>
2014-11-26 19:41:33 -0500

    Add return value to ItemsFromDomExtractor and remove hard-coded className.

--- pinpoint-items-from-dom-extractor.js
+++ pinpoint-items-from-dom-extractor.js
@@ -5,10 +5,11 @@ PinPoint.ItemsfromDomExtractor = function(targetName) {
 // We'll need logic stating what className to use depending on sit:
 // YouTube: "ytp-time-current"
 // Vimeo: "box"
-PinPoint.ItemsfromDomExtractor.prototype = {
-  getTime : function(className) {
-    var timeDivArray = document.getElementsbyClassName("ytp-time-current")
-    var time = timeDivArray[0].innerHTML
 
+PinPoint.ItemsFromDomExtractor.prototype = {
+  getTime : function(className) {
+    var timeDivArray = document.getElementsbyClassName(className);
+    var time = timeDivArray[0].innerHTML;
+    return time;
   }
 }
