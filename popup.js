0a35ba4f099e0a8c1266d336300df9d2debce30d
edwin kim <emoney86@gmail.com>
2014-11-26 23:05:06 -0500

    add event listener for chrome message to get youtube time and url

--- popup.js
+++ popup.js
@@ -1,83 +1,27 @@
-// // Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// // Use of this source code is governed by a BSD-style license that can be
-// // found in the LICENSE file.
-// /**
-//  * Global variable containing the query we'd like to pass to Flickr. In this
-//  * case, kittens!
-//  *
-//  * @type {string}
-//  */
+// This callback function is called when the content script has been
+// injected and returned its results
+function onPageDetailsReceived(pageDetails)  {
+    document.getElementById('website').value = pageDetails.website;
+    document.getElementById('time').value = pageDetails.time;
+    document.getElementById('note').innerText = pageDetails.note;
+}
 
-// var QUERY = 'osprey';
 
-// var kittenGenerator = {
-//   /**
-//    * Flickr URL that will give us lots and lots of whatever we're looking for.
-//    *
-//    * See http://www.flickr.com/services/api/flickr.photos.search.html for
-//    * details about the construction of this URL.
-//    *
-//    * @type {string}
-//    * @private
-//    */
-//   searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-//       'method=flickr.photos.search&' +
-//       'api_key=457150f40ba0085d0c2c9c2e5926d3cb&' +
-//       'text=' + encodeURIComponent(QUERY) + '&' +
-//       'safe_search=1&' +
-//       'content_type=1&' +
-//       'sort=interestingness-desc&' +
-//       'per_page=20',
+// POST the data to the server using XMLHttpRequest
 
-//   /**
-//    * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-//    * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
-//    *
-//    * @public
-//    */
-//   requestKittens: function() {
-//     var req = new XMLHttpRequest();
-//     req.open("GET", this.searchOnFlickr_, true);
-//     req.onload = this.showPhotos_.bind(this);
-//     req.send(null);
-//   },
 
-//   *
-//    * Handle the 'onload' event of our kitten XHR request, generated in
-//    * 'requestKittens', by generating 'img' elements, and stuffing them into
-//    * the document for display.
-//    *
-//    * @param {ProgressEvent} e The XHR ProgressEvent.
-//    * @private
-   
-//   showPhotos_: function (e) {
-//     var kittens = e.target.responseXML.querySelectorAll('photo');
-//     for (var i = 0; i < kittens.length; i++) {
-//       var img = document.createElement('img');
-//       img.src = this.constructKittenURL_(kittens[i]);
-//       img.setAttribute('alt', kittens[i].getAttribute('title'));
-//       document.body.appendChild(img);
-//     }
-//   },
+// When the popup HTML has loaded
+window.addEventListener('load', function() {
+    statusDisplay = document.getElementById('note-display');
+    document.getElementById('add-note').addEventListener('click', "submit", function(){
+        console.log("button linked!")
+    });
 
-//   /**
-//    * Given a photo, construct a URL using the method outlined at
-//    * http://www.flickr.com/services/api/misc.urlKittenl
-//    *
-//    * @param {DOMElement} A kitten.
-//    * @return {string} The kitten's URL.
-//    * @private
-//    */
-//   constructKittenURL_: function (photo) {
-//     return "http://farm" + photo.getAttribute("farm") +
-//         ".static.flickr.com/" + photo.getAttribute("server") +
-//         "/" + photo.getAttribute("id") +
-//         "_" + photo.getAttribute("secret") +
-//         "_s.jpg";
-//   }
-// };
-
-// // Run our kitten generation script as soon as the document's DOM is ready.
-// document.addEventListener('DOMContentLoaded', function () {
-//   kittenGenerator.requestKittens();
-// });
+    // Get the event page
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        // Call the getPageInfo function in the event page, passing in
+        // our onPageDetailsReceived function as the callback. This injects
+        // content.js into the current tab's HTML
+        eventPage.getPageDetails(onPageDetailsReceived);
+    });
+});
