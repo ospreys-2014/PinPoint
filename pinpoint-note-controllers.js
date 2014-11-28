eb37dbb86968c83b783a0061181d76ef20747403
beccapaige <rebeccapaige1216@gmail.com>
2014-11-28 18:35:59 -0500

    Created addNote function

--- pinpoint-note-controllers.js
+++ pinpoint-note-controllers.js
@@ -2,6 +2,10 @@ PinPoint.NoteController = function(){
 	this.notes = []
 };
 
+PinPoint.NoteController.addNote = function(note){
+	this.notes.push(note);
+};
+
 PinPoint.NoteController.getTime = function(pageDetails){
 	var time = pageDetails.time;
 	return time;
