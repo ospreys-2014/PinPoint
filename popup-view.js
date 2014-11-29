a65f4c283334d3df5fb387ae18bec9962b47ed5e
Corey Nilan <cnilan@gmail.com>
2014-11-29 18:14:42 -0500

    Create view.

--- popup-view.js
+++ popup-view.js
@@ -1,35 +1,19 @@
 PinPoint.View = function(){
   //Steven's storeListDOMRoot is equivalent to our noteList
   this.noteListDOMRoot = document.getElementsByTagName('table');
-
 }
 
-//the 'notes' argument will be an array of parsed JSON note objects
 PinPoint.View.prototype = {
-  loadNotesFromDatabase: function(notes) {
-    // this.noteList.appendChild(newNode)
-    controller = new PinPoint.NoteController(notes);
-    //At this point you can call 'controller.notes' and get an array of all the note objects.
-    for (i=0, i <= controller.length, i++) {
-
-    }
-
-  }
-
-}
-
-
-
-//Steven's code that we definitely need:
 //dataSource is the controller
-populateNoteList: function(dataSource) {
-  var noteListParent = this.noteListDOMRoot;
+  populateNoteList: function(dataSource) {
+    var noteListParent = this.noteListDOMRoot;
 
-  while (noteListParent.firstChild) {
-      noteListParent.removeChild(noteListParent.firstChild);
-    }
-  dataSource.noteList.forEach(function(note) {
-    var newNode = new PinPointApp.NotePresenter(note).present();
-    noteListParent.appendChild(newNode);
-  });
+    while (noteListParent.firstChild) {
+        noteListParent.removeChild(noteListParent.firstChild);
+      }
+    dataSource.noteList.forEach(function(note) {
+      var newNode = new PinPointApp.NotePresenter(note).present();
+      noteListParent.appendChild(newNode);
+    });
+  }
 }
