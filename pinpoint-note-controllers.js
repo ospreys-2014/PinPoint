9d31f5f39f1350d74b529eb618c8cd397b6edf5b
Corey Nilan <cnilan@gmail.com>
2014-11-29 17:45:31 -0500

    Change NoteController to accept 'notes' as argument and concat 'notes' into array.

--- pinpoint-note-controllers.js
+++ pinpoint-note-controllers.js
@@ -1,9 +1,11 @@
 var note = {noteTime: "", timeUrl: ""} //jenbex testing note controller functions without lack of note object blocking us
-
-PinPoint.NoteController = function(){
-	this.notes = []
-	this.time = ""
+//LocalStorage is parsed into note objects and passed into the
+//NoteController to be shown in the pop-up.
+PinPoint.NoteController = function(notes){
+	this.notes = [];
+	this.notes = this.notes.concat(notes)
 };
+
 //pushed new notes into notes array
 PinPoint.NoteController.storeNote = function(note){
 	this.notes.push(note);
