1d21b0d4944d6371cf7687bb55a088615cb5bcf3
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-29 22:43:46 -0500

    create function that searches localStorage and a function that parses all note objects

--- pinpoint-note.js
+++ pinpoint-note.js
@@ -1,7 +1,7 @@
 PinPoint.Note = function(){
   this.noteTime = "";
   this.timeUrl = "";
-  this.websiteUrl = "https://www.youtube.com/watch?v=70Tfg8nnnMg"; //Call function directly here to fill this value in
+  this.websiteUrl = "https://www.youtube.com/watch?v=70Tfg800000"; //Call function directly here to fill this value in
   this.noteContent = document.getElementById('content').value;
   this.storageKey = this.websiteUrl + "/" + Date.now()
 }
