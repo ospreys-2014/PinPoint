badf75778ea0af1c3e2abb756c681cf33af80af4
Ali Yeysides <ali.yeysides@gmail.com>
2014-12-01 16:51:02 -0500

    Fix local storage to accept array of note objects. Key is URL.

--- JSONparser.js
+++ JSONparser.js
@@ -1,22 +1,23 @@
 function addNote(url, note) {
-  var array = [];
-  if (localStorage[url] != null){
-    array.concat(getNotes(url));
-    array.push(note);
-    localStorage[url] = JSON.stringify(array);
-  } else {
-    localStorage[url] = array;
-    localStorage[url] = JSON.stringify(array);
-  }
+  console.log("yaay")
+  var noteObjects = getNotes(url)
+  console.log("noteObjects before push", noteObjects)
+  noteObjects.push(note)
+  console.log("noteObjects after push", noteObjects)
+  saveNotes(url, noteObjects)
 }
 
 function getNotes(url){
   if (localStorage[url] == null){
-    console.log("in the if of getNotes");
+    console.log("I'm in the IF of getNotes");
     return []
   } else {
     var retrievedObject = localStorage.getItem(url)
-    console.log(retrievedObject);
+    console.log("I'm in the ELSE of getNotes");
     return JSON.parse(retrievedObject)
   }
+}
+
+function saveNotes(url, notes) {
+  localStorage[url] = JSON.stringify(notes)
 }
