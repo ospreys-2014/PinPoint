c6f3d348bea1d3c4eb2b23551e3b52842796492c
Jensen Bouzi <jensen.bouzi@gmail.com>
2014-11-30 19:57:57 -0500

    add spec to test if view is defined

--- jasmine/spec/NotesViewSpec.js
+++ jasmine/spec/NotesViewSpec.js
@@ -1,7 +1,11 @@
 describe("View", function() {
   beforeEach(function() {
     view = new PinPoint.View();
-    controller = new Pinpoint.NoteController()
+    controller = new Pinpoint.NoteController();
+  })
+
+  it("should be defined", function() {
+    expect(view).toBeDefined();
   })
 
 })
