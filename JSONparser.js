a2cbe2816ef8535f78dabf8ecb1cd4a243679c51
Ali Yeysides <ali.yeysides@gmail.com>
2014-12-01 16:52:05 -0500

    remove console.logs

--- JSONparser.js
+++ JSONparser.js
@@ -1,19 +1,14 @@
 function addNote(url, note) {
-  console.log("yaay")
   var noteObjects = getNotes(url)
-  console.log("noteObjects before push", noteObjects)
   noteObjects.push(note)
-  console.log("noteObjects after push", noteObjects)
   saveNotes(url, noteObjects)
 }
 
 function getNotes(url){
   if (localStorage[url] == null){
-    console.log("I'm in the IF of getNotes");
     return []
   } else {
     var retrievedObject = localStorage.getItem(url)
-    console.log("I'm in the ELSE of getNotes");
     return JSON.parse(retrievedObject)
   }
 }
