6cf162b4172e51cc38d5dcf9e51f894aea6a2084
Rebecca Paige Perez <rebeccapaige1216@gmail.com>
2014-11-30 19:06:10 -0500

    Test for #assignUrl

--- jasmine/spec/NoteSpec.js
+++ jasmine/spec/NoteSpec.js
@@ -8,4 +8,13 @@ describe("Note", function(){
   it("should be defined.", function(){
     expect(note).toBeDefined();
   });
+
+  describe("#assignURL", function(){
+    it("should assign note's websiteUrl attribute to new url.", function(){
+      note.assignURL();
+      expect(note.websiteUrl).not.toEqual("");
+      expect(note.websiteUrl).not.toEqual(undefined);
+      expect(note.websiteUrl).not.toEqual(null);
+    });
+  });
 });
