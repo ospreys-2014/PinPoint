5dd650ea49c4652dd37653edeb0291c552fe8991
Rebecca Paige Perez <rebeccapaige1216@gmail.com>
2014-11-30 19:04:50 -0500

    Test for defining note

--- jasmine/spec/NoteSpec.js
+++ jasmine/spec/NoteSpec.js
@@ -1,1 +1,11 @@
-NoteSpec.js
+describe("Note", function(){
+  beforeEach(function() {
+    note = new PinPoint.Note();
+    localStorage["time"] = "0:30"
+    localStorage["url"] = "http://www.youtube.com/12i3t47grfi"
+  });
+
+  it("should be defined.", function(){
+    expect(note).toBeDefined();
+  });
+});
