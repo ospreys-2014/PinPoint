7c801dc1869a6403f3bf021210bbcde007662ee1
Jensen Bouzi <jensen.bouzi@gmail.com>
2014-11-30 22:31:23 -0500

    add spec 'it should be defined'

--- jasmine/spec/NotesPresenterSpec.js
+++ jasmine/spec/NotesPresenterSpec.js
@@ -1,1 +1,14 @@
-NotesPresenterSpec.js
+describe("NotePresenter", function(){
+  beforeEach(function() {
+    dummyNote = {
+      noteTime: "0:30",
+      noteContent: "I'm a dummy note"
+    };
+    notePresenter = new PinPoint.NotePresenter(dummyNote);
+  });
+
+  it("should be defined.", function(){
+    expect(notePresenter).toBeDefined();
+  });
+
+});
