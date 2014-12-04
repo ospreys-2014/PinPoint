9624bc86a9a2fbbde4ddc9e26f6d6b9ab832beff
beccapaige <rebeccapaige1216@gmail.com>
2014-12-03 20:40:28 -0500

    Added about us page for future editing

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
 window.addEventListener('load', function() {
   var dashLink = document.getElementById("dash-link")
   dashLink.addEventListener("click", function(){
-    console.log("Clicked");
     chrome.tabs.create({url: "dashboard.html"})
+  })
+
+  var aboutLink = document.getElementById("about-link")
+  aboutLink.addEventListener("click", function(){
+    chrome.tabs.create({url: "aboutus.html"})
   })
 });
 
