72a8b7afa4f74a54b4b330e6baeb3e7913b36469
edwin kim <emoney86@gmail.com>
2014-12-04 11:27:55 -0500

    clean up all the files, deleted white space, console logs, commented code, and useless files

--- content.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.sendMessage({
-  'website': document.URL,
-  'time': document.getElementsByClassName('ytp-time-current')[0].innerHTML,
-  'seconds': document.querySelector("video").currentTime,
-});
