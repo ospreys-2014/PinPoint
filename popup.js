06a48fbe85f568130369c6f8243a02648abf2a13
Corey Nilan <cnilan@gmail.com>
2014-12-20 19:43:37 -0500

    Add event listener for clicking enable/disable link (skeleton, unfinished logic) and assign backgroundpage to variable for access.

--- popup.js
+++ popup.js
@@ -1,39 +1,46 @@
 var PinPoint = PinPoint || {};
 
-PinPoint.updatePopup = function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    // Pull the url from the current tab
-    var url = tabs[0].url;
-    // Create array of note objects belonging to current url; returns empty array if no notes present.
-    var notes = getNotes(url);
-    // Sorts the notes by time of video
-    notes.sort(function(a,b) { return a.seconds - b.seconds } );
-
-    var table = document.getElementById('notes-table');
-    table.innerHTML = '';
-
-    for (note of notes) {
-      var node = new PinPoint.NotePresenter(note).present();
-      table.appendChild(node);
-    }
-
-    var links = document.getElementsByClassName("link");
-      for(var i=0;i< links.length; i++) {
-        links[i].addEventListener("click", tabUpdate(i));
-      };
-      function tabUpdate(i) {
-        return function(){
-        chrome.tabs.update(null, {url: links[i].href});
-        };
-      };
-  });
-};
-
-
 window.addEventListener('load', function() {
+  var backgroundPage = chrome.extension.getBackgroundPage()
+
   var dashLink = document.getElementById("dash-link")
   dashLink.addEventListener("click", function(){
     chrome.tabs.create({url: "dashboard.html"})
   })
+
+  var disableLink = document.getElementById("disable")
+  dashLink.addEventListener("click", function(){
+    
+  })
 });
+// PinPoint.updatePopup = function() {
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//     // Pull the url from the current tab
+//     var url = tabs[0].url;
+//     // Create array of note objects belonging to current url; returns empty array if no notes present.
+//     var notes = getNotes(url);
+//     // Sorts the notes by time of video
+//     notes.sort(function(a,b) { return a.seconds - b.seconds } );
+
+//     var table = document.getElementById('notes-table');
+//     table.innerHTML = '';
+
+//     for (note of notes) {
+//       var node = new PinPoint.NotePresenter(note).present();
+//       table.appendChild(node);
+//     }
+
+//     var links = document.getElementsByClassName("link");
+//       for(var i=0;i< links.length; i++) {
+//         links[i].addEventListener("click", tabUpdate(i));
+//       };
+//       function tabUpdate(i) {
+//         return function(){
+//         chrome.tabs.update(null, {url: links[i].href});
+//         };
+//       };
+//   });
+// };
+
+
 
