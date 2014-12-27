ffa3583901e46d28daee39128c2a13a95ddf5b36
Corey Nilan <cnilan@gmail.com>
2014-12-27 13:36:29 -0500

    Create event listener for disable link

--- popup.js
+++ popup.js
@@ -1,17 +1,21 @@
 var PinPoint = PinPoint || {};
 
 window.addEventListener('load', function() {
-  var backgroundPage = chrome.extension.getBackgroundPage()
 
-  var dashLink = document.getElementById("dash-link")
+  var dashLink = document.getElementById("dash-link");
   dashLink.addEventListener("click", function(){
-    chrome.tabs.create({url: "dashboard.html"})
-  })
+    chrome.tabs.create({url: "dashboard.html"});
+  });
 
-  var disableLink = document.getElementById("disable")
-  dashLink.addEventListener("click", function(){
-    
-  })
+  var disableLink = document.getElementById("disable");
+  disableLink.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id, {method: "panel status"}, function(response) {
+          console.log(response);
+      });
+    });
+  });
 });
 // PinPoint.updatePopup = function() {
 //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
