a50e57c645aa2147eeff33207db3da1eef586ef7
Corey Nilan <cnilan@gmail.com>
2014-12-27 13:37:20 -0500

    Remove commented out code.

--- popup.js
+++ popup.js
@@ -17,34 +17,7 @@ window.addEventListener('load', function() {
     });
   });
 });
-// PinPoint.updatePopup = function() {
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-//     // Pull the url from the current tab
-//     var url = tabs[0].url;
-//     // Create array of note objects belonging to current url; returns empty array if no notes present.
-//     var notes = getNotes(url);
-//     // Sorts the notes by time of video
-//     notes.sort(function(a,b) { return a.seconds - b.seconds } );
 
-//     var table = document.getElementById('notes-table');
-//     table.innerHTML = '';
-
-//     for (note of notes) {
-//       var node = new PinPoint.NotePresenter(note).present();
-//       table.appendChild(node);
-//     }
-
-//     var links = document.getElementsByClassName("link");
-//       for(var i=0;i< links.length; i++) {
-//         links[i].addEventListener("click", tabUpdate(i));
-//       };
-//       function tabUpdate(i) {
-//         return function(){
-//         chrome.tabs.update(null, {url: links[i].href});
-//         };
-//       };
-//   });
-// };
 
 
 
