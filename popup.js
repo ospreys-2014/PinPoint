6a1a1418a5953910526054f3568e56646b0b94ed
aliyeysides <ali.yeysides@gmail.com>
2015-01-24 00:00:09 -0500

    remove pointless code in popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
 var PinPoint = PinPoint || {};
 
 window.addEventListener('load', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {method: "panel status"});
-  });
-
   var dashLink = document.getElementById("dash-link");
   dashLink.addEventListener("click", function(){
     chrome.tabs.create({url: "dashboard.html"});
