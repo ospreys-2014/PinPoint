a2c093b909811da883c4fc54c7ffc1b384f7401c
aliyeysides <ali.yeysides@gmail.com>
2015-01-23 23:40:26 -0500

    moved the enable event listeners and chrome runtime listener to popup.js

--- JSONparser.js
+++ JSONparser.js
@@ -49,38 +49,5 @@ Array.prototype.clean = function(deleteValue) {
   return this;
 };
 
-// event listeners for enable and disable feature on popup
-window.onload = function(){
-  var onButton = document.getElementById('on');
-  var offButton = document.getElementById('off');
-
-  // sets default state of app after intall to true
-  if (localStorage.enabled === undefined) {
-    localStorage.enabled = true;
-  }
-  // sets enabled in localStorage to true
-  onButton.addEventListener('click', function(){
-    localStorage["enabled"] = true
-  });
-  // sets enabled in localStorage to false
-  offButton.addEventListener('click', function(){
-    localStorage["enabled"] = false
-  });
-}
-
-// Chrome API event listener
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-  var enabled = localStorage.enabled === "true"
-  if (message.method === "add note"){
-    addNote(message.url, message.note);
-  }
-  else if (message.method === "remove note"){
-    removeNote(message.url, message.seconds);
-  }
-  sendResponse({notesArray: getNotes(message.url), enable: enabled});
-});
-
-// Chrome API icon event listener
-chrome.browserAction.setPopup({popup: "popup.html"});
 
 
